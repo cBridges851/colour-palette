bccbad74d7dcb9cb595a5c0a1ab74de32b1c19b9
Christa Bridges <christa@pebblepad.co.uk>
2021-02-20 13:32:30 +0000

    kivy-version Changed title of window.

--- src/Kivy Version/colour_palette.py
+++ src/Kivy Version/colour_palette.py
@@ -3,6 +3,7 @@ from kivy.uix.label import Label
 
 class ColourPaletteApp(App):
     def build(self):
+        self.title = "Chrispy Colour Palette"
         return Label(text="Hello World")
 
 if __name__ == "__main__":
