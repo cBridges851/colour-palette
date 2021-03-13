beeb49e80d903510b1971ebc2fb16e85e0b305d8
Christa Bridges <christa@pebblepad.co.uk>
2021-03-13 17:58:54 +0000

    conversion-class-connection Updated exception blocks.

--- src/tkinter_version/renderer.py
+++ src/tkinter_version/renderer.py
@@ -166,7 +166,9 @@ class Renderer():
             self.green_input.insert(0, rgb_values[1])
             self.blue_input.delete(0, "end")
             self.blue_input.insert(0, rgb_values[2])
-        except:
+        except tk.TclError:
+            pass
+        except ValueError:
             pass
 
     def rgb_updated(self, event):
@@ -180,7 +182,9 @@ class Renderer():
             )
             self.hex_input.delete(0, "end")
             self.hex_input.insert(0, hex_value)
-        except:
+        except tk.TclError:
+            pass
+        except ValueError:
             pass
 
 Renderer().render()
