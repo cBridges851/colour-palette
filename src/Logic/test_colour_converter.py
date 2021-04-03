6d1917eea72923e22e5ac7059b7bf79f4be9bfca
Christa Bridges <c_bridgescjss@outlook.com>
2021-04-03 11:43:45 +0100

    class-refactor Wrote unit tests for 6 and 3 units (hex to rgb), as well as for without hashtags.

--- src/Logic/test_colour_converter.py
+++ src/Logic/test_colour_converter.py
@@ -11,7 +11,7 @@ class TestColourConverter(unittest.TestCase):
         
         self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("#2955C0"), [41, 85, 192])
         self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("#32A852"), [50, 168, 82])
-        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("#EB4034"), [234, 64, 52])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("#EB4034"), [235, 64, 52])
         self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("#4287F5"), [66, 135, 245])
         self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("#FCBA03"), [252, 186, 3])
     
@@ -21,15 +21,37 @@ class TestColourConverter(unittest.TestCase):
         self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("#123"), [17, 34, 51])
         self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("#ABC"), [170, 187, 204])
         self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("#1BC"), [17, 187, 204])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("#BC9"), [187, 204, 153])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("#B5E"), [187, 85, 238])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("#45E"), [68, 85, 238])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("#F12"), [255, 17, 34])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("#5D6"), [85, 221, 102])
 
 
-    # def test_without_hashtags(self):
-    #     self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("FFFFFF"), [255, 255, 255])
-    #     self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("FFFF"), [255, 255, 255])
-    #     self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("FFF"), [255, 255, 255])
-    #     self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("000000"), [0, 0, 0])
-    #     self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("0000"), [0, 0, 0])
-    #     self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("000"), [0, 0, 0])
+
+    def test_without_hashtags(self):
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("FFFFFF"), [255, 255, 255])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("000000"), [0, 0, 0])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("112233"), [17, 34, 51])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("AABBCC"), [170, 187, 204])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("11BBCC"), [17, 187, 204])
+        
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("2955C0"), [41, 85, 192])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("32A852"), [50, 168, 82])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("EB4034"), [235, 64, 52])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("4287F5"), [66, 135, 245])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("FCBA03"), [252, 186, 3])
+
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("FFF"), [255, 255, 255])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("000"), [0, 0, 0])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("123"), [17, 34, 51])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("ABC"), [170, 187, 204])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("1BC"), [17, 187, 204])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("BC9"), [187, 204, 153])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("B5E"), [187, 85, 238])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("45E"), [68, 85, 238])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("F12"), [255, 17, 34])
+        self.assertAlmostEqual(ColourConverter().convert_hex_to_rgb("5D6"), [85, 221, 102])
 
 
 
