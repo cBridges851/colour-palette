eaf6d7ad374d1bc6333b12df144d37041a86ce77
Christa Bridges <c_bridgescjss@outlook.com>
2021-04-05 11:44:07 +0100

    system-tray The application can now be accessed from the system tray, but the system tray code needs to be moved into its own file.

--- src/tkinter_version/renderer.py
+++ src/tkinter_version/renderer.py
@@ -1,5 +1,28 @@
 import tkinter as tk
 from colour_converter import ColourConverter
+from PySide2 import QtWidgets, QtGui
+import os
+import sys
+
+class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
+    def __init__(self, icon, parent=None):
+        QtWidgets.QSystemTrayIcon.__init__(self, icon, parent)
+        self.setToolTip("Chrispy Colour Palette")
+        menu = QtWidgets.QMenu(parent)
+
+        # Context Menu
+        open_colour_palette = menu.addAction("Open palette")
+        open_colour_palette.triggered.connect(self.open_colour_palette_app)
+        
+        _exit = menu.addAction("Exit")
+        _exit.triggered.connect(lambda: sys.exit())
+
+        menu.addSeparator()
+        self.setContextMenu(menu)
+        self.activated.connect(self.open_colour_palette_app)
+
+    def open_colour_palette_app(self):
+        Renderer().render()
 
 class Renderer():
     """
@@ -217,6 +240,12 @@ class Renderer():
             self.hex_input.delete(0, "end")
             self.hex_input.insert(0, hex_value)
             
-        
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    widget = QtWidgets.QWidget()
+    tray_icon = SystemTrayIcon(QtGui.QIcon("favicon.ico"), widget)
+    tray_icon.show()
+    sys.exit(app.exec_())
 
-Renderer().render()
+if __name__ == "__main__":
+    main()
