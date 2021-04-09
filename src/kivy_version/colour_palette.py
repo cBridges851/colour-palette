27714af07303c7df1ee598ce8ac16681ea16026d
Christa Bridges <christa@pebblepad.co.uk>
2021-04-09 18:22:29 +0100

    interface-redesign Redesigned the application so it only shows one type of conversion at a time as it's not possible for the a text box to change while another is being typed into.

--- src/kivy_version/colour_palette.py
+++ src/kivy_version/colour_palette.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle
 from kivy.graphics import Color
+from colour_converter import ColourConverter
 
 class Interface(Widget):
     """
@@ -10,9 +11,12 @@ class Interface(Widget):
     def __init__(self, **kwargs):
         super(Interface, self).__init__(**kwargs)
         
-        with self.canvas:
-            Color(1, 0, 0, 1, mode="rgba")
-            self.colour_display = Rectangle(pos=(300, 360), size=(200,200))
+        # with self.canvas:
+        #     Color(1, 0, 0, 1, mode="rgba")
+        #     self.colour_display = Rectangle(pos_hint=(0, 1), size=(200,200))
+
+    def convert(self):
+        print("example has been triggered")
 
 class ColourPaletteApp(App):
     """
