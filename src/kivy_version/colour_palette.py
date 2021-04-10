020b3ae268d93f386e125f733b66c5bbe2af8dfc
Christa Bridges <christa@pebblepad.co.uk>
2021-04-10 11:03:38 +0100

    redesign-and-logic Created the rgb screen and connected it to the conversion class. About to start the hex to rgb work.

--- src/kivy_version/colour_palette.py
+++ src/kivy_version/colour_palette.py
@@ -1,22 +1,50 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.graphics import Rectangle
-from kivy.graphics import Color
 from colour_converter import ColourConverter
-
-class Interface(Widget):
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+class Interface(Screen):
     """
         Renders the interface of the application
     """
+    red_input = ObjectProperty(None)
+    green_input = ObjectProperty(None)
+    blue_input = ObjectProperty(None)
+    output = ObjectProperty(None)
+
     def __init__(self, **kwargs):
         super(Interface, self).__init__(**kwargs)
-        
         # with self.canvas:
         #     Color(1, 0, 0, 1, mode="rgba")
         #     self.colour_display = Rectangle(pos_hint=(0, 1), size=(200,200))
 
-    def convert(self):
-        print("example has been triggered")
+    def convert_rgb_to_hex(self):
+        red = self.red_input
+        green = self.green_input
+        blue = self.blue_input
+        rgb_inputs = [red, green, blue]
+
+        for rgb_input in rgb_inputs:
+            if rgb_input.text.isdigit() is False:
+                rgb_input.background_color = "#FF726F"
+            else:
+                rgb_input.background_color = "#FFFFFF"
+
+        hex_value = ColourConverter.convert_rgb_to_hex(
+            self,
+            red.text,
+            green.text,
+            blue.text
+        )
+
+        # Output on interface
+        if hex_value == "Invalid":
+            self.output.color = "#FF0000"
+            self.output.text = hex_value
+            return
+        
+        self.output.color = "#FFFFFF"
+        self.output.text = f"#{hex_value}"
 
 class ColourPaletteApp(App):
     """
